fac170d98212b696a305cb05d3af589171211266
Ramon Augusto <augustoramon471@gmail.com>
2021-06-19 15:55:30 -0300

    layout página inicial e mock de detalhes

---
+++ DesafioProway/DesafioProway/Controller/Loans/EmprestimoGarantia/EmprestimoGarantiaViewController.swift
@@ -0,0 +1,30 @@
+//
+//  EmprestimoGarantiaViewController.swift
+//  DesafioProway
+//
+//  Created by Caio Berkley on 18/06/21.
+//  Copyright © 2021 Ramon Augusto. All rights reserved.
+//
+
+import UIKit
+
+class EmprestimoGarantiaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ DesafioProway/DesafioProway/Controller/Loans/ImpostoDeRenda/ImpostoDeRendaViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ImpostoDeRendaViewController.swift
+//  DesafioProway
+//
+//  Created by Caio Berkley on 18/06/21.
+//  Copyright © 2021 Ramon Augusto. All rights reserved.
+//
+
+import UIKit
+
+class ImpostoDeRendaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
