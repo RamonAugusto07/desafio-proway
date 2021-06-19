46fb87d9e465928edb5d253dbccd973671af29e2
Ramon Augusto <augustoramon471@gmail.com>
2021-06-19 18:53:14 -0300

    lógica da tela inicial e layout da lista de empréstimos

--- DesafioProway/DesafioProway/Controller/InitialViewController/ViewController.swift
+++ DesafioProway/DesafioProway/Controller/InitialViewController/ViewController.swift
@@ -9,23 +9,33 @@
 import UIKit
 
 class InitialViewController: UIViewController {
-
-    //MARK: Outlets
-    @IBOutlet weak var StartOutlet: UIButton!
     
+    //MARK: Outlets
+    @IBOutlet weak var startOutlet: UIButton!
+    @IBOutlet weak var textCpf: UITextField!
+    @IBOutlet weak var cpfAlertLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-    }
-
-    //MARK: Action
-    @IBAction func StartAction(_ sender: UIButton) {
-        
-        let nextView = ListViewController()
-        self.present(nextView, animated: true, completion: nil)
         
     }
     
+    func verifyCpf() {
+        if self.textCpf.text?.isEmpty == true {
+            self.cpfAlertLabel.isHidden = false
+            
+        } else {
+            self.cpfAlertLabel.isHidden = true
+            
+            let nextView = ListViewController()
+            self.present(nextView, animated: true, completion: nil)
+            
+        }
+    }
+    
+    //MARK: Action
+    @IBAction func startAction(_ sender: UIButton) {
+        verifyCpf()
+        
+    }
 }
-
